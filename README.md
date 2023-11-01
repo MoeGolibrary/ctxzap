dcd68b7a9dbc91b9e409645bec78d287f6dd9ebe
acrazing <joking.young@gmail.com>
2023-11-01 23:17:13 +0800

    docs: add readme

--- README.md
+++ README.md
@@ -1,2 +1,39 @@
-# grpc_zap
-bind zap with grpc context
+# ctxzap
+
+Bind zap to context, built-in support for grpc.
+
+## Quick start
+
+1. Install
+
+   ```bash
+   go get github.com/MoeGolibrary/ctxzap
+   ```
+
+2. Bootstrap
+
+    ```go
+    logger := zap.NewExample()
+    _ = grpc.NewServer(
+        grpc.ChainUnaryInterceptor(
+            grpc_zap.UnaryServerInterceptor(logger, grpc_zap.WithAccessLog(true, true)),
+        ),
+        grpc.ChainStreamInterceptor(
+            grpc_zap.StreamServerInterceptor(logger, grpc_zap.WithEventLog(true, true)),
+        ),
+    )
+
+    ```
+
+3. Logging
+
+    ```go
+    func (s xxxServer) Ping(ctx context.Context) (xxx, error) {
+        ctxzap.Info("Hello world", zap.String("id", "1"))
+    }
+
+    ```
+
+## License
+
+MIT
--- ctxzap.go
+++ ctxzap.go
@@ -27,16 +27,19 @@ func FromContext(ctx context.Context) *zap.Logger {
 	return nil
 }
 
+// ToContext put logger to context
 func ToContext(ctx context.Context, logger *zap.Logger, fields []zap.Field) context.Context {
 	return context.WithValue(ctx, zapCtxKey, &ctxLogger{logger: logger, fields: fields})
 }
 
+// AddFields attach more fields to the context
 func AddFields(ctx context.Context, fields ...zap.Field) {
 	if logger, ok := ctx.Value(zapCtxKey).(*ctxLogger); ok && logger != nil {
 		logger.fields = append(logger.fields, fields...)
 	}
 }
 
+// ReplaceField replace existing field or append new field
 func ReplaceField(ctx context.Context, field zap.Field) {
 	if logger, ok := ctx.Value(zapCtxKey).(*ctxLogger); ok && logger != nil {
 		for i, f := range logger.fields {
@@ -49,24 +52,28 @@ func ReplaceField(ctx context.Context, field zap.Field) {
 	}
 }
 
+// Debug log
 func Debug(ctx context.Context, msg string, fields ...zap.Field) {
 	if logger, ok := ctx.Value(zapCtxKey).(*ctxLogger); ok && logger != nil {
 		logger.logger.Debug(msg, append(logger.fields, fields...)...)
 	}
 }
 
+// Info log
 func Info(ctx context.Context, msg string, fields ...zap.Field) {
 	if logger, ok := ctx.Value(zapCtxKey).(*ctxLogger); ok && logger != nil {
 		logger.logger.Info(msg, append(logger.fields, fields...)...)
 	}
 }
 
+// Warn log
 func Warn(ctx context.Context, msg string, fields ...zap.Field) {
 	if logger, ok := ctx.Value(zapCtxKey).(*ctxLogger); ok && logger != nil {
 		logger.logger.Warn(msg, append(logger.fields, fields...)...)
 	}
 }
 
+// Error log
 func Error(ctx context.Context, msg string, fields ...zap.Field) {
 	if logger, ok := ctx.Value(zapCtxKey).(*ctxLogger); ok && logger != nil {
 		logger.logger.Error(msg, append(logger.fields, fields...)...)
